05889a7b4b1ec4ff3c8cb6b711a09a6a0d27b340
Ross Affandy <affandy@affandy.net>
2013-11-08 17:05:27 +0800

    modified class

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
 
-class HelloWorld() {
+class HelloWorld
+{
+    /**
+     * @var PDO
+     */
+    private $pdo;
 
- public function __construct() {
- }
- 
- public function hello() {
-	echo 'hello travis';
- }
- 
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function hello($what = 'World')
+    {
+        $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
+        $this->pdo->query($sql);
+        return "Hello $what";
+    }
+
+
+    public function what()
+    {
+        $sql = "SELECT what FROM hello";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchColumn();
+    }
 }
-
-$test = new HelloWorld();
-$test->hello;
